f593fc1f12ef44acb13c3ea711621efd06da3be2
AlexanderZezin <alexanderzezin@gmail.com>
2024-05-16 15:12:24 +0300

    Поправить тесты. Добавить проверку изменения/добавления фамилии в профиле пользователя.

--- website_django/settings.py
+++ website_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = Setting.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
