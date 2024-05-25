8cb1ad6fae92254e1c5f35933c293bc865652c51
AlexanderZezin <alexanderzezin@gmail.com>
2024-05-26 02:31:17 +0300

    Поправить модель User: обязательное, уникальное,  индексируемое поле email

--- users/models.py
+++ users/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True, db_index=True, blank=False)
     is_email = models.BooleanField(default=False, verbose_name='is_email')
